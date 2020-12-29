9cd182689026486423ea0213e41d37194ced595e
BASARANOMO <39518094+BASARANOMO@users.noreply.github.com>
2020-12-29 23:33:59 +0100

    Correct bugs

--- code_Julia/utils.jl
+++ code_Julia/utils.jl
@@ -6,15 +6,20 @@ function create_graph(G_original, dims, us, fs, J_init, J_fin, e)
     cost_dispatch = dims.γ
     
     G = SimpleDiGraph((3 * U + 4 * F) * J + U + F + 1)
-    capacity = zeros((3 * U + 4 * F) * J + U + F + 1, (3 * U + 4 * F) * J + U + F + 1)
-    cost_mat = zeros((3 * U + 4 * F) * J + U + F + 1, (3 * U + 4 * F) * J + U + F + 1)
+    capacity = zeros((3 * U + 4 * F) * J + U + F + 1, 
+        (3 * U + 4 * F) * J + U + F + 1)
+    cost_mat = zeros((3 * U + 4 * F) * J + U + F + 1, 
+        (3 * U + 4 * F) * J + U + F + 1)
     
-    for j = J_init:J_fin # for each day
+    for j = 1:J # for each day
         for u = 1:U # connect each u to each f stock
             for f = 1:F
-                add_edge!(G, (j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 1, (j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 1)
-                capacity[(j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 1, (j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 1] += Inf
-                cost_mat[(j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 1, (j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 1] += G_original.d[u, U + f] * cost_dispatch
+                add_edge!(G, (j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 1, 
+                    (j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 1)
+                capacity[(j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 1, 
+                    (j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 1] += Inf
+                cost_mat[(j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 1, 
+                    (j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 1] += G_original.d[u, U + f] * cost_dispatch
             end
         end
         
@@ -29,48 +34,66 @@ function create_graph(G_original, dims, us, fs, J_init, J_fin, e)
         end
     end
     
-    for j = J_init:J_fin
+    for j = 1:J
         for u = 1:U
             # connect u day j to two additional vertices
-            add_edge!(G, (j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 1, (j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 2)
-            add_edge!(G, (j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 1, (j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 3)
+            add_edge!(G, (j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 1, 
+                (j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 2)
+            add_edge!(G, (j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 1, 
+                (j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 3)
             
-            capacity[(j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 1, (j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 2] += us[u].r[e, j]
-            capacity[(j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 1, (j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 3] += Inf
+            capacity[(j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 1, 
+                (j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 2] += us[u].r[e, j + J_init - 1]
+            capacity[(j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 1, 
+                (j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 3] += Inf
             
-            cost_mat[(j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 1, (j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 3] += us[u].cs[e]
+            cost_mat[(j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 1, 
+                (j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 3] += us[u].cs[e]
             
-            if j < J_fin
+            if j < J
                 # connect two additional vertices to u day j+1
-                add_edge!(G, (j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 2, j * (3 * U + 4 * F) + 3 * (u - 1) + 1)
-                add_edge!(G, (j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 3, j * (3 * U + 4 * F) + 3 * (u - 1) + 1)
+                add_edge!(G, (j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 2, 
+                    j * (3 * U + 4 * F) + 3 * (u - 1) + 1)
+                add_edge!(G, (j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 3, 
+                    j * (3 * U + 4 * F) + 3 * (u - 1) + 1)
                 
-                capacity[(j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 2, j * (3 * U + 4 * F) + 3 * (u - 1) + 1] += us[u].r[e, j]
-                capacity[(j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 3, j * (3 * U + 4 * F) + 3 * (u - 1) + 1] += Inf
+                capacity[(j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 2, 
+                    j * (3 * U + 4 * F) + 3 * (u - 1) + 1] += us[u].r[e, j + J_init - 1]
+                capacity[(j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 3, 
+                    j * (3 * U + 4 * F) + 3 * (u - 1) + 1] += Inf
             end
         end
         
         for f = 1:F
             # connect f stock day j to two additional vertices
-            add_edge!(G, (j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 1, (j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 2)
-            add_edge!(G, (j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 1, (j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 3)
+            add_edge!(G, (j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 1, 
+                (j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 2)
+            add_edge!(G, (j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 1, 
+                (j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 3)
             
-            capacity[(j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 1, (j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 2] += fs[f].r[e, j]
-            capacity[(j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 1, (j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 3] += Inf
+            capacity[(j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 1, 
+                (j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 2] += fs[f].r[e, j + J_init - 1]
+            capacity[(j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 1, 
+                (j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 3] += Inf
             
-            cost_mat[(j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 1, (j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 3] += fs[f].cs[e]
+            cost_mat[(j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 1, 
+                (j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 3] += fs[f].cs[e]
             
-            if j < J_fin
+            if j < J
                 # connect each f stock to f consommation of the next day
                 add_edge!(G, (j - 1) * (3 * U + 4 * F) + 3 * U + 4 * f, j * (3 * U + 4 * F) + 3 * U + 4 * f)
                 capacity[(j - 1) * (3 * U + 4 * F) + 3 * U + 4 * f, j * (3 * U + 4 * F) + 3 * U + 4 * f] += Inf
                 
                 # connect two additional vertices to f day j+1
-                add_edge!(G, (j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 2, j * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 1)
-                add_edge!(G, (j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 3, j * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 1)
+                add_edge!(G, (j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 2, 
+                    j * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 1)
+                add_edge!(G, (j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 3, 
+                    j * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 1)
                 
-                capacity[(j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 2, j * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 1] += fs[f].r[e, j]
-                capacity[(j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 3, j * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 1] += Inf
+                capacity[(j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 2, 
+                    j * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 1] += fs[f].r[e, j + J_init - 1]
+                capacity[(j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 3, 
+                    j * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 1] += Inf
             end
         end
     end
@@ -102,15 +125,15 @@ function set_demand(dims, us, fs, J_init, J_fin, e) # b parameter
     # J = dims.J
     demand = zeros((3 * U + 4 * F) * J + U + F + 1)
     
-    for j = J_init:J_fin
+    for j = 1:J
         # for each u
         for u = 1:U
-            demand[(j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 1] -= us[u].b⁺[e, j]
+            demand[(j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 1] -= us[u].b⁺[e, j + J_init - 1]
         end
         
         # for each f consommation
         for f = 1:F
-            demand[(j - 1) * (3 * U + 4 * F) + 3 * U + 4 * f] += fs[f].b⁻[e, j]
+            demand[(j - 1) * (3 * U + 4 * F) + 3 * U + 4 * f] += fs[f].b⁻[e, j + J_init - 1]
         end
     end
     
@@ -200,4 +223,36 @@ function run_opt(g, dims, us, fs, J_init, J_fin, e, optimizer)
     demand = set_demand(dims, us, fs, J_init, J_fin, e)
     flow = min_cost_flow(g, demand, capacity, cost_mat, optimizer)
     return flow
+end
+
+function read_flow(flow, U, F, J_init, J_fin)
+    # create a U * F * J matrix (for a given e) for dispatching
+    # create a U * J matrix (for a given e) for stockage
+    # create a F * J matrix (for a given e) for stockage
+    J = J_fin - J_init + 1
+    dispatch = zeros(U, F, J)
+    stock_U = zeros(U, J)
+    stock_F = zeros(F, J)
+    for j = 1:J
+        for u = 1:U
+            for f = 1:F
+                dispatch[u, f, j] += flow[(j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 1,
+                    (j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 1]
+            end
+        end
+        
+        for u = 1:U
+            stock_U[u, j] = flow[(j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 1, (j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 2] +
+            flow[(j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 1, (j - 1) * (3 * U + 4 * F) + 3 * (u - 1) + 3]
+        end
+        
+        for f = 1:F
+            stock_F[f, j] = flow[(j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 1,
+                (j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 2] + 
+            flow[(j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 1,
+                (j - 1) * (3 * U + 4 * F) + 3 * U + 4 * (f - 1) + 3]
+        end
+    end
+    
+    return dispatch, stock_U, stock_F
 end
